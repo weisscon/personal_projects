8be0021600186dd11f671c4c3ae5e1aa99a927bc
weisscon <145080350+weisscon@users.noreply.github.com>
2023-09-14 23:31:08 -0400

    Add files via upload

---
+++ ShakespeareFight/inputTools.py
@@ -0,0 +1,46 @@
+'''
+Conor Weiss
+10/07/22
+CS 5001, Fall 2022
+Lab 4: Patterns with While loops
+
+A library for input validation tools that I can continue to use through the rest of the course.
+
+Functions included are:
+1. float_input_validation function
+2. bounded_integer_input_validation function
+'''
+
+'''
+float input validation function
+inputs: 1 string
+prints string and gets an input.  Will not advance until user gives input that can be a float
+returns 1 float
+'''
+
+def float_input_validation(prompt:str):
+    user_input = 'a'
+    while (bool(type(user_input) == str)):
+        try:
+            user_input = float(input(prompt))
+        except ValueError:
+            print("Input must be a number")
+    return(user_input)
+
+'''
+bounded integer input validation function
+inputs: 2 integers, 1 string
+prints string and gets an input.  Will not advance until user gives a number between the upper and lower bounds
+returns 1 integer
+'''
+
+def bounded_integer_input_validation(upper_bound:int, lower_bound:int, prompt:str):
+    user_input = -10000
+    while (user_input<lower_bound or user_input > upper_bound):
+        try:
+             user_input = int(input(prompt))
+        except ValueError:
+            user_input = -10000
+        if (user_input<lower_bound or user_input > upper_bound):
+            print("Please enter a number between " + str(lower_bound) +" and " + str(upper_bound) + ".")
+    return(user_input)
